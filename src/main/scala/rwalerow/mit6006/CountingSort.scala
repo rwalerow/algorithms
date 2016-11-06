2ab82ecce620cbedeb84c34ec32047fed32c9c57
Robert <viniolli@gmail.com>
2016-11-06 14:36:54 +0100

    Add order preserving count sort

--- src/main/scala/rwalerow/mit6006/CountingSort.scala
+++ src/main/scala/rwalerow/mit6006/CountingSort.scala
@@ -1,5 +1,7 @@
 package rwalerow.mit6006
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by robert on 06.11.16.
   */
@@ -20,4 +22,25 @@ object CountingSort {
 
     result
   }
+
+  def sort[A](arr: Array[ElementWithKey[A]], k: Int): Array[ElementWithKey[A]] = {
+    val counting: Array[ArrayBuffer[A]] = Array.fill(k + 1){ ArrayBuffer[A]() }
+
+    arr.foreach(e => counting(e.key).append(e.value))
+
+    val result: Array[ElementWithKey[A]] = Array.ofDim(arr.length)
+
+    var iter = 0
+
+    counting.indices.foreach{ c =>
+      if(counting(c).nonEmpty)
+       counting(c).foreach { e =>
+         result(iter) = ElementWithKey(c, e)
+         iter += 1
+       }
+    }
+    result
+  }
+
+  case class ElementWithKey[A](key: Int, value: A)
 }
--- src/test/scala/rwalerow/mit6006/CountingSortSpec.scala
+++ src/test/scala/rwalerow/mit6006/CountingSortSpec.scala
@@ -1,6 +1,7 @@
 package rwalerow.mit6006
 
 import org.scalatest.{FlatSpec, Matchers}
+import rwalerow.mit6006.CountingSort.ElementWithKey
 
 /**
   * Created by robert on 06.11.16.
@@ -13,5 +14,23 @@ class CountingSortSpec extends FlatSpec with Matchers {
     val result = CountingSort.sort(arr, 52)
 
     result should contain theSameElementsInOrderAs Array(1,2,2,3,3,4,5,6,45,52)
+  }
+
+  "Sort" should "include additional data" in {
+    val arr = Array(
+      ElementWithKey(5, "a"), ElementWithKey(9, "o"),
+      ElementWithKey(2, "z"), ElementWithKey(5, "w"),
+      ElementWithKey(4, "p"), ElementWithKey(2, "a")
+    )
+
+    val expected = Array(
+      ElementWithKey(2, "z"), ElementWithKey(2, "a"),
+      ElementWithKey(4, "p"), ElementWithKey(5, "a"),
+      ElementWithKey(5, "w"), ElementWithKey(9, "o")
+    )
+
+    val result = CountingSort.sort(arr, 9)
+
+    result should contain theSameElementsInOrderAs expected
   }
 }
