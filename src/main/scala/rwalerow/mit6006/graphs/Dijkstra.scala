94a0860fb901d5fc5555d499cbed29f3684438ce
Robert <viniolli@gmail.com>
2016-12-02 12:45:41 +0100

    Added Dijkstra schema

---
+++ src/main/scala/rwalerow/mit6006/graphs/Dijkstra.scala
@@ -0,0 +1,12 @@
+package rwalerow.mit6006.graphs
+
+import rwalerow.mit6006.graphs.Graph.Graph
+
+/**
+  * Created by robert on 02.12.16.
+  */
+class Dijkstra {
+
+  def paths[K](graph: Graph[K]): Map[K, Int] = ???
+
+}
